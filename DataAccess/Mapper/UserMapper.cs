8f8869cdef1ce02f8400e21260740f6370595dd1
jjimenezg19 <47197113+jjimenezg19@users.noreply.github.com>
2025-03-31 17:13:52 -0500

    adding mapper

---
+++ DataAccess/Mapper/UserMapper.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using DataAccess.Dao;
+using DTO;
+
+namespace DataAccess.Mapper
+{
+	public class UserMapper : ICrudStatements, IObjectMapper
+	{
+        public List<BaseClass> BuildObjects(List<Dictionary<string, object>> objectRows)
+        {
+            List<BaseClass> users = new List<BaseClass>();
+
+            foreach (var row in objectRows)
+            {
+                var user = BuildObject(row);
+                users.Add(user);
+            }
+
+            return users;
+        }
+
+        public BaseClass BuildObject(Dictionary<string, object> row)
+        {
+            var user = new Usuario();
+
+            if (row.ContainsKey("role_id") && int.TryParse(row["role_id"].ToString(), out int userId))
+                user.Id = userId;
+
+            if (row.ContainsKey("role_id") && int.TryParse(row["role_id"]))
+
+           
+
+            return null;
+        }
+
+        public MySqlOperation GetCreateStatement(BaseClass entityDTO)
+        {
+            throw new NotImplementedException();
+        }
+
+        public MySqlOperation GetDeleteStatement(BaseClass entityDTO)
+        {
+            throw new NotImplementedException();
+        }
+
+        public MySqlOperation GetRetrieveAllStatement()
+        {
+            throw new NotImplementedException();
+        }
+
+        public MySqlOperation GetRetrieveByIdStatement(int Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public MySqlOperation GetUpdateStatement(BaseClass entityDTO)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+
